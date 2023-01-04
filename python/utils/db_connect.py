077bf5d71d49ec03be53b9c31b06747533d5868d
EvanSat <53355852+EvanSat@users.noreply.github.com>
2023-01-04 07:03:48 -0600

    Update db_connect.py

--- python/utils/db_connect.py
+++ python/utils/db_connect.py
@@ -4,24 +4,26 @@ from config import db_config as db_cfg
 
 
 def sql_query(query: str):
+    try:
+        db_connection = psycopg2.connect(host=db_cfg.hostname,
+                                         user=db_cfg.username,
+                                         password=db_cfg.password,
+                                         dbname=db_cfg.database,
+                                         port=db_cfg.port)
 
-    db_connection = psycopg2.connect(host=db_cfg.hostname,
-                                     user=db_cfg.username,
-                                     password=db_cfg.password,
-                                     dbname=db_cfg.database,
-                                     port=db_cfg.port)
+        cursor = db_connection.cursor()
+        cursor.execute(query)
 
-    cursor = db_connection.cursor()
-    cursor.execute(query)
+        # shows the connection query
+        print(cursor.fetchall())
+        db_connection.commit()
+    except (Exception, psycopg2.Error) as error:
+        print(f'Failed to run query due to: {error}')
 
-    # shows the connection query
-    print(cursor.fetchall())
-
-    cursor.close()
-    db_connection.commit()
-    db_connection.close()
-    
-    return 0
+    finally:
+        if db_connection:
+            cursor.close()
+            db_connection.close()
 
 
 # sql_query('SELECT * FROM quantrix_schema.tran_fact')
